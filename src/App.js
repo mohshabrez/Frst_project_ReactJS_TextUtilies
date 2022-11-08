6f07ad5d26f766c60dcaa660a07229662fc6d7fa
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2022-11-08 18:09:30 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './componets/Navbar';
+import About from './componets/About';
+import TextForm from './componets/TextForm';
+import React , {useState} from 'react';
+import Alert from './componets/Alert';
+// import Switch from 'react-router-dom'
+import{
+  HashRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link 
+} from "react-router-dom";
+// import About from './componets/About';
+
+
 
 function App() {
+  const [mode, setmode] = useState('light');
+  const [alert, setalert] = useState(null);
+
+  const showAlert=(message, type) => {
+      setalert({
+        msg: message,
+        type:type
+      })
+      setTimeout(() => {
+        setalert(null)
+      },1500);
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setmode('dark')
+      document.body.style.backgroundColor="grey"
+      showAlert("Dark Mode has been enabled","Success")
+      document.title = "TextUtilies-DarkMode"
+      setInterval(()=>{
+        document.title = "TextUtilies is Amazing" 
+      },2000)
+      setInterval(()=>{
+        document.title = "Install TextUtilies" 
+      },1500)
+    }
+    else{
+      setmode('light')
+      document.body.style.backgroundColor="white"
+      showAlert("Light Mode has been enabled","Success")
+      document.title = "TextUtilies-LightMode"
+      setInterval(()=>{
+        document.title = "TextUtilies is Amazing" 
+      },2000)
+      setInterval(()=>{
+        document.title = "Install TextUtilies" 
+      },1500)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Router>
+      <Routes>  
+       <Switch>
+          <Route path='/about'>
+           <About/>
+          </Route>
+          <Route path='/'>
+          
+            </Route>
+        </Switch>
+    {/* <About/> */}
+        {/*
+      </Routes>
+    </Router> */}
+     <div className="container my-3">
+     <Navbar  title="textUtilies" about="About us" mode={mode} toggleMode={toggleMode} />
+     <Alert alert={alert}/>
+     <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} toggleMode={toggleMode} />
+     </div>
+   </>
   );
 }
 
 export default App;
+
+
+// const [mode, setMode] = useState('light');
+
+// const toggleMode = () => {
+//   if(mode === 'light'){
+//     setMode('dark')
+//     document.body.style.backgroundColor = 'grey';
+//   } 
+//   else{
+//     setMode('light')
+//     document.body.style.backgroundColor = 'white';
+//   }mode={mode} toggleMode={toggleMode}
